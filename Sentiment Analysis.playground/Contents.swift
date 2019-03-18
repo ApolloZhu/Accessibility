e94aa1683d87a9a016f37fabd4abb152e1ae1e30
Zhiyu Zhu/朱智语 <public-apollonian@outlook.com>
2019-03-17 23:16:23 -0400

    Enable line by line execution

--- Sentiment Analysis.playground/Contents.swift
+++ Sentiment Analysis.playground/Contents.swift
@@ -4,16 +4,16 @@
  1. download training data from [Sentiment140](http://help.sentiment140.com/for-students/)
  2. unzip it
  3. drag and drop the 2 csv files to the `Resources` folder you see on the left
+ - Experiment: Ready? Let's begin!
  */
 import Cocoa
 import CreateML
 import NaturalLanguage
-//: - Experiment: Ready? Let's begin!
-do {
+import PlaygroundSupport
 //: ## Importing Datasets
-func dataTable(fromFile url: URL) throws -> MLDataTable {
+func dataTable(fromFile url: URL) -> MLDataTable {
 //: We can create a data table from a Sentiment140 csv file
-    var table = try MLDataTable(
+    var table = try! MLDataTable(
         contentsOf: url,
 //: But we should inform Core ML the csv doesn't have a header,
 //: so it will use default column names: X1, X2, ...
@@ -32,16 +32,19 @@ func dataTable(fromFile url: URL) throws -> MLDataTable {
     }, named: "Label")
     return table
 }
+//: Now we can load the data tables:
+let trainingData = dataTable(fromFile: #fileLiteral(resourceName: "training.1600000.processed.noemoticon.csv"))
+let testData = dataTable(fromFile: #fileLiteral(resourceName: "testdata.manual.2009.06.14.csv"))
 //: ## Creating Maching Learning Model
 //: Now we have a way to generate datatables,
 //: we can create a text classifier from the dataset
-let sentimentClassifier = try MLTextClassifier(
-    trainingData: try dataTable(fromFile: #fileLiteral(resourceName: "training.1600000.processed.noemoticon.csv")),
+let sentimentClassifier = try! MLTextClassifier(
+    trainingData: trainingData,
     textColumn: "Text",
     labelColumn: "Label"
 )
 //: ... and evaluate it with the test dataset
-let evaluationMetrics = sentimentClassifier.evaluation(on: try dataTable(fromFile: #fileLiteral(resourceName: "testdata.manual.2009.06.14.csv")))
+let evaluationMetrics = sentimentClassifier.evaluation(on: testData)
 //: ## Check Our Results
 //: Let's see some statistics
 let trainingAccuracy = (1 - sentimentClassifier.trainingMetrics.classificationError) * 100
@@ -53,23 +56,20 @@ Validation Accuracy: \(validationAccuracy)%
 Evaluation Accuracy: \(evaluationAccuracy)%
 """)
 //: - Experiment: Why don't we test it with some comments about this playground?
-print(try sentimentClassifier.prediction(from: "This looks fantastic"))
+print(try! sentimentClassifier.prediction(from: "This looks fantastic"))
 //: ## Exporting MLModel
 //: Now we've verified it, let's save the model with some metadata to describe it:
 let metadata = MLModelMetadata(
 //: - Note: Change author to **your name**. In my case, I'm Zhiyu Zhu, so:
     author: "Zhiyu Zhu",
     shortDescription: "A model trained to classify sentiment",
-    version: "1.0",
 //: - Important: We should pay tribute to Sentiment140 as we used their dataset:
-    additional: ["Bibliography":"A. Go, R. Bhayani, and L. Huang, “Twitter Sentiment Classification Using Distant Supervision,” in CS224N Project Report, 2009."]
+    license: "A. Go, R. Bhayani, and L. Huang, “Twitter Sentiment Classification Using Distant Supervision,” in CS224N Project Report, 2009.",
+    version: "1.0"
 )
 //: Save the trained model as `Sentiment140.mlmodel`
-let url = Bundle.main.resourceURL!.appendingPathComponent("Sentiment140.mlmodel")
+let url = playgroundSharedDataDirectory.absoluteURL.appendingPathComponent("Sentiment140.mlmodel")
 try sentimentClassifier.write(to: url, metadata: metadata)
-//: Lastly, open it in Finder so we can use it later!
+//: Lastly, open it so we can use it later!
 NSWorkspace.shared.open(url)
-//: Now we're all set. Have a nice day!
-} catch {
-    print(error)
-}
+//: We're all set. Have a nice day!
