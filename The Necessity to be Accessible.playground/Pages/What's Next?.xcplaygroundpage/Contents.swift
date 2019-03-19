dbef416701a8de8642f282eac4e18b26b3733f42
Apollo Zhu <public-apollonian@outlook.com>
2019-03-19 03:50:59 -0400

    Finish "Where to go from here?"

---
+++ The Necessity to be Accessible.playground/Pages/What's Next?.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+/*:
+ # Where to go from here?
+ 
+ Congratulations! Now you've learned some basics to make your apps more accessible. Go ahead and audit
+ **all** your apps--on iOS, macOS, watchOS, tvOS, and even the webpage for your app--for accessibility.
+ You should refer to resources like [Human Interface Guidelines](https://developer.apple.com/design/human-interface-guidelines/ios/app-architecture/accessibility/)
+ and [Web Content Accessibility Guidelines (WCAG)](https://www.w3.org/WAI/standards-guidelines/wcag/).
+ Share with your friends and your team, let them also join us in improving our world to allow
+ ***everyone*** to benefit from technologies the ***same*** as anyone else.
+ */
+//: ---
+//: ## Feedback
+//: Thank you for exploring this Playground. How do you feel like it?
+//: - Callout(Privacy Policy): Your review is **NOT** shared with anyone and is only evaluated locally on your computer, so feel free to type how you truly feel.
+Your_review_for_this_Playground("""
+I like it. Thank you for sharing this!
+""")
+//: - Note: For more details on how the machine learning model that reasons about your review is created using Create ML and the Natural Language framework, see `Sentiment Analysis.playground` in the `Resources` folder of this page.
