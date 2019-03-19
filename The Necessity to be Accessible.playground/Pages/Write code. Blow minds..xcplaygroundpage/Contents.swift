ac4aa02a4958390e6bf46708622a25a403773373
Zhiyu Zhu/朱智语 <public-apollonian@outlook.com>
2019-03-19 01:59:16 -0400

    Stash Changes

--- The Necessity to be Accessible.playground/Pages/Write code. Blow minds..xcplaygroundpage/Contents.swift
+++ The Necessity to be Accessible.playground/Pages/Write code. Blow minds..xcplaygroundpage/Contents.swift
@@ -1,4 +1,6 @@
 //: - Important: You may want to disable live view should you have motion sickness.
+//: - Callout(Scenario): Great! It's WWDC again, and you want to celebrate it with a cool animation you design by writing code to 
+#warning("Zoom Animation")
 
 import Foundation
 
