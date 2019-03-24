f9558844ddd5f276128ca73a307b5c6ad51b0a43
Zhiyu Zhu/朱智语 <public-apollonian@outlook.com>
2019-03-24 15:01:24 -0400

    ClassKit
    No time for verification, please don't crash, thank you

--- The Necessity to be Accessible.playground/Pages/Share this playground.xcplaygroundpage/Contents.swift
+++ The Necessity to be Accessible.playground/Pages/Share this playground.xcplaygroundpage/Contents.swift
@@ -1,14 +1,105 @@
 /*:
  # Teaching Others about Accessibility
  
- I hope you have enjoyed this playground, and is eager to allow more people to
+ I hope you have enjoyed this playground
+ */
+Playground.current
+/*: ... and is eager to allow more people to
  learn about accessibility through this then go out to support people with needs.
  
  One way to do that is adopting ClassKit, so teachers can assign it to students,
  should we have modified this to fit into an app.
  */
 import ClassKit
-CLSDataStore.shared.mainAppContext
-
-
+//: ## Expose Contents
+public class ClassKitContentProvider: NSObject, CLSDataStoreDelegate {
+//: Starting from iOS 12.2, you'll be able to create an app extension
+//: with base class conforming to `CLSContextProvider` to handle the creation.
+    public func makeAvailableToClassKit() {
+        for page in Playground.current.pages {
+            if let sections = page.sections {
+                for section in sections {
+                    CLSDataStore.shared.mainAppContext
+                        .descendant(matchingIdentifierPath: section.identifierPath,
+                                    completion: { _,_ in })
+                }
+            } else {
+                CLSDataStore.shared.mainAppContext
+                    .descendant(matchingIdentifierPath: page.identifierPath,
+                                completion: { _,_ in })
+            }
+        }
+    }
+//: Listen for context creation
+    public static let shared = ClassKitContentProvider()
+    private override init() {
+        super.init()
+        CLSDataStore.shared.delegate = self
+    }
+    public func createContext(forIdentifier identifier: String,
+                              parentContext: CLSContext,
+                              parentIdentifierPath: [String]) -> CLSContext? {
+//: Find a node in the playground structure based on the identifier path.
+        let identifierPath = parentIdentifierPath + [identifier]
+//: Find the node to create create context with in playground.
+        guard let pageIdentifier = identifierPath.first,
+            let page = Playground.current.pages.first(where: { $0.identifier == pageIdentifier }),
+            let node = page.descendant(matching: Array(identifierPath[1...]))
+            else { return nil }
+//: Create our context with information found.
+        let context = CLSContext(type: node.contextType,
+                                 identifier: identifier,
+                                 title: node.title)
+        context.topic = .computerScienceAndEngineering
+//: Use universal link to locate the content.
+//: - Note: You can also use user activity
+        identifierPath.joined(separator: "/")
+            .addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
+            .map { context.universalLinkURL = URL(string: "ntba://" + $0) }
+//: Save our context!
+        return context
+    }
+}
+//: ---
+//:
+//: ## Record Learning Activity
+import UIKit
+//: We'll want to report the progress back to teachers, thereby creating
+class PlaygroundPage: UIViewController, UIScrollViewDelegate {
+    var page: Playground.Page!
+//: Whenever user starts reading a playground page
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        page.withContext { context in
+//: We inform the teacher that student has started reading this chapter
+            context.becomeActive()
+//: ... and start recording their progress.
+            (context.currentActivity ?? context.createNewActivity()).start()
+        }
+    }
+//: We'll update the progress as they play around with this accessibility tutorial
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let midY = scrollView.contentOffset.y + scrollView.frame.size.height
+        let progress = midY / scrollView.contentSize.height
+        page.withContext { context in
+            context.currentActivity?.progress = min(max(Double(progress), 0), 1)
+        }
+    }
+//: And when they stop reading,
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        page.withContext {
+//: we inform the teacher how many lines of code were edited
+            let numberOfLinesChanged = CLSQuantityItem(
+                identifier: "lines-edited",
+                title: NSLocalizedString("Lines Edited", comment: "Activity report title")
+            )
+            numberOfLinesChanged.quantity = 5
+            $0.currentActivity?.primaryActivityItem = numberOfLinesChanged
+//: ... and stop recording this student activity.
+            $0.currentActivity?.stop()
+            $0.resignActive()
+        }
+    }
+}
 //: [Previous](@previous) | [➜ Next: Next Steps](@next)
---
+++ The Necessity to be Accessible.playground/Pages/Share this playground.xcplaygroundpage/Sources/Playground.swift
@@ -0,0 +1,144 @@
+import ClassKit
+
+public protocol Node {
+    var parent: Node? { get }
+    var children: [Node]? { get }
+    var identifier: String { get }
+    var title: String { get }
+    var contextType: CLSContextType { get }
+}
+
+extension Node {
+    public var identifierPath: [String] {
+        var pathComponents = [identifier]
+        if let parent = parent {
+            pathComponents = parent.identifierPath + pathComponents
+        }
+        return pathComponents
+    }
+    
+    /// Finds a node in the play list hierarchy by its identifier path.
+    public func descendant(matching identifierPath: [String]) -> Node? {
+        guard let identifier = identifierPath.first else { return self }
+        return children?.first(where: { $0.identifier == identifier })?
+            .descendant(matching: Array(identifierPath[1...]))
+    }
+}
+
+public struct Playground {
+    public struct Page {
+        public struct Section {
+            public let title: String
+            fileprivate(set) var id: Int!
+            fileprivate(set) var page: Page!
+            init(title: String) {
+                self.title = title
+            }
+        }
+        let id: Int
+        public let title: String
+        public private(set) var sections: [Section]?
+        private static var nextId = 0
+        init(title: String, sections: [Section]?) {
+            self.id = Page.nextId
+            Page.nextId += 1
+            self.title = title
+            self.sections = sections
+            let newSections = self.sections?.enumerated().map {
+                (id, oldSection) -> Section in
+                var section = oldSection
+                section.id = id
+                section.page = self
+                return section
+            }
+            self.sections = newSections
+        }
+    }
+    public let pages: [Page]
+}
+
+extension Playground.Page: Node {
+    public var parent: Node? { return nil }
+    public var children: [Node]? { return sections }
+    public var identifier: String { return "page-\(id)" }
+    public var contextType: CLSContextType { return .chapter }
+}
+
+extension Playground.Page.Section: Node {
+    public var parent: Node? { return page }
+    public var children: [Node]? { return nil }
+    public var identifier: String { return "section-\(id!)" }
+    public var contextType: CLSContextType { return .section }
+}
+
+let structure: [(page: String, sections: [String])] = [
+    (NSLocalizedString("Write code. Blow minds.", comment: ""), []),
+    (NSLocalizedString("Intro to Accessibility", comment: ""), [
+        NSLocalizedString("What is \"Accessibility\"?", comment: ""),
+        NSLocalizedString("Basics of been Accessible", comment: "")
+        ]),
+    (NSLocalizedString("Colors", comment: ""), [
+        NSLocalizedString("More than just Colors", comment: ""),
+        NSLocalizedString("Semantics, with or without Color", comment: "")
+        ]),
+    (NSLocalizedString("Universal Design", comment: ""), [
+        NSLocalizedString("Designed for EVERYONE", comment: ""),
+        NSLocalizedString("Localization", comment: ""),
+        NSLocalizedString("RTL and Text Display", comment: ""),
+        NSLocalizedString("Numbers and Formatters", comment: ""),
+        NSLocalizedString("Date and Calendars", comment: "")
+        ]),
+    (NSLocalizedString("It works, AND convenient.", comment: ""), [
+        NSLocalizedString("No good design, but Excellent Design", comment: ""),
+        NSLocalizedString("Aggregate", comment: ""),
+        NSLocalizedString("Disect", comment: ""),
+        NSLocalizedString("Fine Tune", comment: "")
+        ]),
+    (NSLocalizedString("Share this playground", comment: ""), [
+        NSLocalizedString("Expose Contents", comment: ""),
+        NSLocalizedString("Record Learning Activity", comment: ""),
+        ]),
+    (NSLocalizedString("What's Next?", comment: ""), [
+        NSLocalizedString("Where to go from here?", comment: ""),
+        NSLocalizedString("Feedback", comment: "")
+        ]),
+]
+
+extension Playground {
+    public static let current = Playground(pages: structure.map {
+        return Playground.Page(title: $0.page, sections: $0.sections.map {
+            return Playground.Page.Section(title: $0)
+        })
+    })
+}
+
+extension Node {
+    /// We could have used `activeContext` and `runningActivity`, but
+    /// in cases where there are more than one context and activity,
+    /// that'll not do what we intended. Therefore implemention is lookup.
+    public func withContext(do process: @escaping (CLSContext) -> Void) {
+        CLSDataStore.shared.mainAppContext.descendant(matchingIdentifierPath: identifierPath)
+        { (context, error) in
+            guard let context = context else {
+                return print(error?.localizedDescription ?? "Unknown CLSError")
+            }
+            process(context)
+            CLSDataStore.shared.save { (error) in
+                guard let error = error else { return }
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension Playground: CustomPlaygroundDisplayConvertible {
+    public var playgroundDescription: Any {
+        return pages.reduce("") {
+            (result: String, page: Playground.Page) -> String in
+            let sections: String = page.sections?.reduce("", {
+                return $0 + "\t- \($1.title)\n"
+            }) ?? ""
+            return result + "- \(page.title)\n" + sections
+        }
+    }
+}
